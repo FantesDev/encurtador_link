47e0d6e2c8c2a5c0086d3e28415ec2532c5ef165
FantesDev <vitorhenriquefante81@gmail.com>
2024-05-22 17:58:07 -0300

    incluindo o contato nas routes

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/login', [App\Http\Controllers\AccountController::class, 'login'])->
 
 Route::get('/logout', [App\Http\Controllers\AccountController::class, 'logout'])->name('logout');
 
+Route::get('/contato', [App\Http\Controllers\AccountController::class, 'contato'])->name('contato');
+
 Route::prefix('/app')->group(function () {
     Route::get('/generate', [App\Http\Controllers\LinkController::class, 'generate'])->name('generate');
     Route::get('/remove', [App\Http\Controllers\LinkController::class, 'remove'])->name('remove');
